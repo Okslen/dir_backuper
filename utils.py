0b03889f6cdb5f53bc60affc35351ff81fb3e44d
Игорь Дроздовский <okslen@gmail.com>
2024-07-08 17:33:59 +0300

    add try_func decorator for network functions

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-
+import functools
 import os
 import shutil
 
@@ -8,35 +8,32 @@ from logger import get_logger
 logger = get_logger(__name__)
 
 
+def try_func(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except FileNotFoundError as err:
+            logger.error(f'{err.strerror} {err.filename}')
+        except PermissionError as err:
+            logger.error(f'{err.strerror} {err.filename}')
+    return wrapper
+
+
+@try_func
 def make_copy(src: str, dst: str):
-    try:
-        shutil.copy2(src, dst)
-    except FileNotFoundError as err:
-        logger.error(f'{err.strerror} {err.filename}')
+    shutil.copy2(src, dst)
 
 
+@try_func
 def make_dir(dir_path: str) -> str:
-    try:
-        os.makedirs(dir_path, exist_ok=True)
-    except FileNotFoundError as err:
-        logger.error(f'{err.strerror} {err.filename}')
+    os.makedirs(dir_path, exist_ok=True)
 
 
 def get_path(path: str, deep: int) -> str:
     return path if deep == 0 else '\\'.join(path.split('\\')[:-deep])
 
 
+@try_func
 def get_scandir(path: str):
-    try:
-        return os.scandir(path)
-    except FileNotFoundError as err:
-        logger.error(f'{err.strerror} {err.filename}')
-
-
-def try_func(func, *args, **kwargs):
-    try:
-        return func(*args, **kwargs)
-    except FileNotFoundError as err:
-        logger.error(f'{err.strerror} {err.filename}')
-    except PermissionError as err:
-        logger.error(f'{err.strerror} {err.filename}')
+    return os.scandir(path)
